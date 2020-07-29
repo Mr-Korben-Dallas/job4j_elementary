e531f9159dfcc97c630320fcad25b1103d1d6f5f
Mr-Korben-Dallas <daco.inc.v@gmail.com>
2020-07-29 18:20:09 +0400

    Пофиксил все ошибки выданные CheckStyle. [#335126]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -6,14 +6,9 @@ public class ArgMethod {
         System.out.println("Hello, " + name);
     }
 
-
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
-
         ArgMethod.hello(name);
-
     }
-
-
 }
