336546d21ea8c116acbb8a5bce58f4ad06e9102e
Mr-Korben-Dallas <daco.inc.v@gmail.com>
2020-07-30 18:38:36 +0400

    Реализовал тестирование в классе Converter. [#335149]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,10 +13,14 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        int dollar = Converter.rubleToDollar(200);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("200 rubles are " + dollar + " dollar.");
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        int outD = rubleToDollar(in);
+        boolean passedD = expected == outD;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("140 rubles are 2. Test result : " + passedD);
     }
 
 }
