f4ea8affdd8473913a5056fb092713c83399a5e6
Mr-Korben-Dallas <daco.inc.v@gmail.com>
2020-07-31 21:22:47 +0400

    Написал тест для класса SqArea. [#335153]

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenSqArea62Then2() {
+        int p = 6;
+        int k = 2;
+        double expected = 2.0;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
