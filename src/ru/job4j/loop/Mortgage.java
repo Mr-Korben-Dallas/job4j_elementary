710a0126bed5ddcb4ba69c2bcd8ecf4a8656a417
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2021-05-23 13:55:41 +0400

    Исправлен класс Mortgage  [#156315 #159454]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,17 +3,12 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        boolean isPaid = false;
 
-        while (!isPaid) {
+        while (amount > 0) {
             year++;
 
             double overpayment = ( amount / 100 ) * percent;
             amount = (amount + overpayment) - salary;
-
-            if (amount < 0) {
-                isPaid = true;
-            }
         }
 
         return year;
